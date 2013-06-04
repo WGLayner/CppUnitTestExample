1a2bcf0d6afc8c06937fe768178b263f9a4ec16c
Nikolaos Dalalelis <ndalalelis@yahoo.com>
2013-06-04 18:58:34 -0400

    Removed precompiled headers

--- CppUnitTestExample/CppUnitTestExample.cpp
+++ CppUnitTestExample/CppUnitTestExample.cpp
@@ -1,7 +1,8 @@
 // CppUnitTestExample.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include <tchar.h>
+#include <iostream>
 #include "WindowsVersion.h"
 
 int _tmain(int argc, _TCHAR* argv[])
